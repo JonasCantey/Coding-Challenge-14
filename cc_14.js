c3ba3d3e2cc68e705782f3465d1c3bba130bb53b
Jonas <jonashooncantey@gmail.com>
2025-03-09 21:46:27 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -18,7 +18,24 @@ function createTicket (customerName, description, priority) {
              const resolveButton = document.createElement('Button'); //creating a remove button
              resolveButton.innerHTML = 'Resolve'; //inside the remove button is the word remove
              ticketCard.appendChild(resolveButton); //this appends the remove button to the ticket card
-         
+                //Task 3
+             if (priority === "High") { //if priority is high then high-priority class is applied to metric card
+                ticketCard.classList.add("high-priority");
+             }
      };
+        //Task 3
+    function highlightHighPriority () { //using the highpriority list we select all cards
+        const highPriorityCards = document.querySelectorAll(".high-priority")
+        const cardsArray = Array.from(highPriorityCards);
 
-createTicket("Mark Garcia", "Computer is bugged out", "High")
+        cardsArray.forEach(card => {    //and apply a border and highlight in lightcoral.
+            card.style.backgroundColor = "lightcoral";
+            card.style.border = "2px solid red";
+        })
+    }
+
+
+
+createTicket("Mark Garcia", "Computer is bugged out", "Low");
+createTicket("Grark Malinda", "Computer is on fire", "High");
+highlightHighPriority();
